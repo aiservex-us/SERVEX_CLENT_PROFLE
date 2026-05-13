e689f675b62e5165d01c6ac70f92a8d895953a18
Prueba <prueba@MacBook-Pro-de-Prueba.local>
2026-05-13 17:04:50 -0500

    sassss

--- app/Home/components/header.jsx
+++ app/Home/components/header.jsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
-import { supabaseGoogle } from '../lib/supabaseClient';
+import { supabaseGoogle } from '../../lib/supabaseClient';
 
 const TeamsFloatingHeader = () => {
   const [user, setUser] = useState(null);
